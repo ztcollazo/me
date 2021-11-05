f123e56acb8e40ea6710b697fe0c78c5e4c5ebe5
Zachary Collazo <ztcollazo08@gmail.com>
2021-11-05 16:53:55 -0400

    Fix page layout (with header transparency bug fix)

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,18 +1,20 @@
-import Link from "next/link";
-import React from "react";
+import { FC } from 'react';
+import Link from 'next/link';
 
-const FooterLink: React.FC<{href: string}> = ({href, children}) => <Link href={href} passHref><a className="hover:underline">{children}</a></Link>
+const FooterLink: FC<{ href: string }> = ({ href, children }) => <Link href={href} passHref><a className="hover:underline">{children}</a></Link>;
 
-const Footer = () => {
-    return (
-        <footer className="absolute left-1/4 right-1/4 text-center bottom-0 p-5">
-            <strong>
-                &#169;2021 Zachary Collazo
-            </strong>  &nbsp; &#183; &nbsp;
-            <FooterLink href="/">Home</FooterLink> &nbsp; &#183; &nbsp;
-            <FooterLink href="/about">About</FooterLink>
-        </footer>
-    )
-}
+const Footer = () => (
+  <footer className="text-center absolute left-0 right-0 bg-transparent bottom-0 w-100% p-5">
+    <strong>
+      &#169;2021 Zachary Collazo
+    </strong>
+    {' '}
+&nbsp; &#183; &nbsp;
+    <FooterLink href="/">Home</FooterLink>
+    {' '}
+&nbsp; &#183; &nbsp;
+    <FooterLink href="/about">About</FooterLink>
+  </footer>
+);
 
 export default Footer;
--- components/NavLink.tsx
+++ components/NavLink.tsx
@@ -1,10 +1,21 @@
-import React from "react";
-import Link, { LinkProps } from "next/link";
-import { useRouter } from "next/router";
+import { FC, AnchorHTMLAttributes } from 'react';
+import Link, { LinkProps } from 'next/link';
+import { useRouter } from 'next/router';
 
-const NavLink = ({href, onClick = null, activeClassName, className, children,  ...rest}: React.PropsWithChildren<LinkProps & { activeClassName?: string, className?: string } & React.AnchorHTMLAttributes<any>>) => {
-    const router = useRouter();
-    return <Link href={href} passHref {...rest}><a {...rest} className={`${router.pathname === href ? `${activeClassName} ` : ""}${className}`} >{children}</a></Link>
-}
+type NavLinkProps = LinkProps & {
+  activeClassName?: string,
+} & AnchorHTMLAttributes<any>;
+
+const NavLink: FC<NavLinkProps> = ({
+  href, onClick, activeClassName, className, children, ...rest
+}) => {
+  const router = useRouter();
+  // eslint-disable-next-line react/jsx-props-no-spreading
+  return <Link href={href} passHref {...rest}><a {...rest} className={`${router.pathname === href ? `${activeClassName} ` : ''}${className}`}>{children}</a></Link>;
+};
+
+NavLink.defaultProps = {
+  activeClassName: '',
+};
 
 export default NavLink;
