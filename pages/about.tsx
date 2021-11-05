af1dec0ec9d9c1e4f7ea0fda37349dd81bc6484a
Zachary Collazo <ztcollazo08@gmail.com>
2021-11-05 16:54:58 -0400

    Change page makeup (with nice parallax effect)

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,41 +1,41 @@
-import { useTheme } from "next-themes";
-import React from "react";
-import { useMediaQuery } from "react-responsive";
-import { RoughNotation } from "react-rough-notation";
-import Typist from "react-typist"
-import { randomColor, random } from "../utils/random";
-import Link from "next/link"
-import Head from "next/head"
+import Typist from 'react-typist';
+import Link from 'next/link';
+import Head from 'next/head';
 
-const About = () => {
-    const [show, setShouldShow] = React.useState(false);
-    const small = useMediaQuery({minWidth: 640});
-    const { theme } = useTheme();
-
-    return (
-        <div className="text-center p-10">
-            <Head>
-                <title>About | Zachary Collazo</title>
-            </Head>
-            <RoughNotation iterations={random(2, 6)} animationDuration={1400} color={randomColor(theme === "dark")} type="box" animate show={show && small}>
-                <main className="flex flex-col text-center flex-wrap flex-1 p-2 md:p-5 font-mono">
-                    <Typist avgTypingDelay={10} stdTypingDelay={5} startDelay={10} cursor={{show:false}} onTypingDone={() => setShouldShow(true)}>
-                        <h1 className="text-5xl p-2 md:p-4 md:text-9xl">Hey there.</h1>
-                        <h1 className="text-3xl p-2 md:p-4 md:text-7xl">
-                            My name is Zachary Collazo
-                        </h1>
-                        <div className="text-xl p-2 md:p-4 md:text-3xl">
-                            I&#39;m a teenage developer from Chesterfield, VA, USA. I make cool websites
-                            and apps, and I strive to learn everything I can get my hands on.
-                        </div>
-                        <div className="text-lg p-2 md:p-4 md:text-2xl">
-                            Check out some of my <Link passHref href="/projects"><a className="font-bold">projects</a></Link>.
-                        </div>
-                    </Typist>
-                </main>
-            </RoughNotation>
+const About = () => (
+  <div className="p-10">
+    <Head>
+      <title>About | Zachary Collazo</title>
+    </Head>
+    <main className="flex flex-col min-h-[77vh] bottom-0 top-0 flex-wrap flex-1 p-2 md:p-5 font-mono">
+      <Typist
+        avgTypingDelay={10}
+        stdTypingDelay={5}
+        startDelay={10}
+        cursor={{ show: false }}
+      >
+        <h1 className="text-5xl font-semibold p-2 md:p-4 md:text-8xl">Hey there!</h1>
+        <h1 className="text-3xl p-2 md:p-4 md:text-7xl">
+          My name is Zachary Collazo.
+        </h1>
+        <div className="text-xl p-2 md:p-4 md:text-3xl">
+          I&#39;m a teenager from Chesterfield, VA. I like making cool websites
+          and apps and whatever else you want me to make.
+          I strive to learn everything I can get my hands on (which is a lot).
+          I&#39;ve always had an inclination to math and science, which has led me
+          to an interest in technology. Like I said, I love to learn, so
+          if you have a suggestion for this website, please open an issue at&nbsp;
+          <a style={{ transitionDuration: '.5s' }} href="//github.com/ztcollazo/me" className="font-bold hover:translate-x-[1px] transition-transform inline-block">this project&#39;s github repo.</a>
         </div>
-    )
-}
+        <div className="text-lg p-2 md:p-4 md:text-2xl">
+          You can also check out some of my other
+          <Link passHref href="/projects">
+            <a style={{ transitionDuration: '.5s' }} className="font-bold inline-block hover:translate-x-[1px] transition-transform">&nbsp;projects.</a>
+          </Link>
+        </div>
+      </Typist>
+    </main>
+  </div>
+);
 
 export default About;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,57 +1,85 @@
-import Head from 'next/head'
-import { RoughNotationGroup, RoughNotation } from 'react-rough-notation';
+import { useState } from 'react';
+import Head from 'next/head';
+import { RoughNotation } from 'react-rough-notation';
 import { useTheme } from 'next-themes';
-import { useMediaQuery } from 'react-responsive';
-import { random, randomColor } from '../utils/random';
+import { Fade } from 'react-reveal';
+import { Link as ScrollLink, Element } from 'react-scroll';
+import { HiOutlineChevronDown } from '@react-icons/all-files/hi/HiOutlineChevronDown';
 import Link from 'next/link';
+import { ParallaxProvider, Parallax } from 'react-scroll-parallax';
+import { randomColor } from '../utils/random';
 
 const Home = () => {
+  // Main animation done, first animation done, etc.
+  const [mad, setMad] = useState(false);
+  const [fad, setFad] = useState(false);
+  const [sad, setSad] = useState(false);
+
   const { theme } = useTheme();
-  const small = useMediaQuery({minWidth: 768});
-  const inBetween = useMediaQuery({minWidth: 1020});
-  const dark = theme === "dark";
+  const dark = theme === 'dark';
 
   return (
-    <div className="overflow-hidden relative z-0 m-0 t-0 left-0 right-0 text-center">
-      <Head>
-        <title>Home | Zachary Collazo</title>
-      </Head>
-      <main className="flex flex-col md:grid m-0 md:grid-cols-1 justify-between pr-36 md:pr-20 md:p-12 items-stretch">
-        <RoughNotation show={small} iterations={random(2, 6)} type="box" animate animationDelay={1400} animationDuration={1000} color={randomColor(dark)}>
-          <div id="jumbotron" className="flex relative md:grid items-stretch justify-between w-full flex-col md:grid-cols-2 font-mono">
-            <RoughNotationGroup> 
-              <div className={`flex w-full md:m-20 m-16 flex-col items-stretch`}>
-                <RoughNotation iterations={random(2, 6)} animate show animationDuration={700} color={randomColor(dark)} strokeWidth={3} type="circle">
-                  <h1 className="md:text-9xl text-5xl py-4">Hello</h1>
-                </RoughNotation>
-                <h2 className="md:text-4xl text-lg p-2 md:p-4">my name is</h2>
-                <RoughNotation multiline iterations={random(2, 6)} animate show animationDuration={700} color={randomColor(dark)} type="highlight">
-                  <h1 className="md:text-6xl text-3xl font-extralight">Zachary Collazo</h1>
-                </RoughNotation>
-                <h1 className="mt-12 rotate-6 md:text-5xl text-2xl font-mono">
-                  <span style={{color: randomColor(dark)}}>I </span>
-                  <span style={{color: randomColor(true)}} className="font-mono dark:bg-gray-700 bg-gray-200 rounded-xl p-2"><code>Code</code></span>
-                  <span style={{color: randomColor(dark)}}> Stuff</span>
-                </h1>
+    <ParallaxProvider>
+      <div>
+        <Head>
+          <title>Home | Zachary Collazo</title>
+        </Head>
+        <div id="parallax" className="flex flex-row">
+          <Parallax y={[-30, 30]}>
+            <main style={{ zIndex: -1 }} className="flex bg-transparent -mt-8 flex-col relative top-0 min-h-screen md:grid m-0 md:grid-cols-1 justify-center pr-36 md:pr-20 md:p-12 pt-0 items-center">
+              <div id="jumbotron" className="flex relative md:grid items-stretch mt-4 justify-between w-full flex-col pt-0 md:grid-cols-2 font-mono">
+                <Fade bottom onReveal={() => setMad(true)}>
+                  <div className="flex w-full md:m-20 md:mt-0 m-16 flex-col items-stretch">
+                    <h1 className="md:text-9xl font-semibold text-5xl py-4">Hello</h1>
+                    <h2 className="md:text-4xl font-extralight text-lg p-2 md:p-4">my name is</h2>
+                    <RoughNotation animationDelay={975} iterations={2} animate show={mad} animationDuration={1100} strokeWidth={7} color={randomColor(dark)} type="underline">
+                      <h1 className="md:text-6xl text-3xl font-extralight">Zachary Collazo</h1>
+                    </RoughNotation>
+                  </div>
+                </Fade>
               </div>
-            </RoughNotationGroup>
-            {/* <div className="text-center md:text-5xl text-2xl font-serif rotate-12">
-              Image will go here eventually
-              <h1>Image Placeholder</h1>
-            </div> */}
+              <div className="absolute bottom-36 left-1/2 right-1/2 w-5 h-5">
+                <ScrollLink smooth duration={500} to="rest">
+                  <HiOutlineChevronDown color="currentColor" />
+                </ScrollLink>
+              </div>
+            </main>
+          </Parallax>
+        </div>
+        <div className="dark:bg-gray-700 h-[20vh] relative left-0 right-0 -top-32 slide-top-clip-path bg-gray-300 w-full" />
+        <Element name="rest">
+          <div className="-mt-56 min-h-[100vh] relative dark:bg-gray-700 p-20 bg-gray-300 flex flex-col">
+            <Fade opposite left wait={600} onReveal={() => setFad(true)}>
+              <div className="min-h-full flex flex-col justify-start items-stretch p-5">
+                <h2 className="text-2xl text-left md:text-5xl">Learn more</h2>
+                <Link href="/about" passHref>
+                  <a className="mt-5">
+                    <h1 style={{ transitionDuration: '1s' }} className="text-4xl relative font-bold text-left md:text-7xl hover:translate-x-1 transition-transform">
+                      &nbsp;&nbsp;
+                      <RoughNotation type="highlight" color={randomColor(dark)} animate show={fad}>About me</RoughNotation>
+                    </h1>
+                  </a>
+                </Link>
+              </div>
+            </Fade>
+            <Fade opposite right wait={600} onReveal={() => setSad(true)}>
+              <div className="min-h-full min-w-full flex flex-col justify-end items-stretch p-20">
+                <h2 className="text-xl text-right md:text-5xl">Check out my</h2>
+                <Link href="/projects" passHref>
+                  <a className="mt-5">
+                    <h1 style={{ transitionDuration: '1s' }} className="text-4xl font-bold text-right relative md:text-7xl transition-transform hover:-translate-x-1">
+                      &nbsp;&nbsp;
+                      <RoughNotation type="highlight" color={randomColor(dark)} animate show={sad}>Projects</RoughNotation>
+                    </h1>
+                  </a>
+                </Link>
+              </div>
+            </Fade>
           </div>
-        </RoughNotation>
-      </main>
-      <div className="bg-transparent h-full grid grid-cols-2 grid-flow-row-dense min-w-screen">
-        <div className="slanted-right h-full absolute left-0 min-w-full">
-          <h2>Slanted-right</h2>
-        </div>
-        <div className="slanted-left h-full w-1/2 absolute right-0 min-w-full bg-gray-300 dark:bg-gray-600">
-          <h2>Slanted-left</h2>
-        </div>
+        </Element>
       </div>
-    </div>
-  )
-}
+    </ParallaxProvider>
+  );
+};
 
 export default Home;
