e04837a4809208ba2104a988c353089ba85c64a4
Zachary Collazo <ztcollazo08@gmail.com>
2022-01-04 17:11:12 -0500

    Fix images

--- next.config.js
+++ next.config.js
@@ -1,15 +1,19 @@
 const withPWA = require("next-pwa")
+const withImages = require("next-images");
+const withPlugins = require("next-compose-plugins");
 const prod = process.env.NODE_ENV === 'production'
 
-module.exports = withPWA({
+module.exports = withPlugins([withPWA, withImages], {
   swcMinify: true,
   reactStrictMode: true,
   pwa: {
     dest: 'public',
     disable: !prod
   },
+  dynamicAssetPrefix: true,
   images: {
-    domains: ['avatars.githubusercontent.com', 'github.com']
+    domains: ['avatars.githubusercontent.com', 'github.com'],
+    disableStaticImages: true,
   },
   webpack(config, {}) {
     config.resolve.fallback = {
