47dd8b6b0e32feab4d881ae88273217cc6dbf2a6
Zachary <ztcollazo08@gmail.com>
2023-02-06 18:47:36 -0500

    Fix a few bugs in the favicon and projects

--- src/utils/meta-for.ts
+++ src/utils/meta-for.ts
@@ -4,12 +4,12 @@ export const nameFor = (name: string): string => {
   if (name === 'ztcollazo') return 'Github profile design';
   if (name === 'me') return 'Portfolio site';
   if (name === 'destination-app') return 'Destination';
-  if (name === 'mako-browser') return 'Mako';
   if (name === 'joke.svg') return 'Joke.SVG';
   return humanizeString(name);
 };
 
 export const ownerFor = (name: string): string => {
   if (name === 'destination-app') return 'ericcecchi';
+  if (name === '404-Pathmaking-Tools' || name === 'FRC-2023-Charged-Up') return 'FRC-404-NOT-FOUND';
   return 'ztcollazo';
 };
